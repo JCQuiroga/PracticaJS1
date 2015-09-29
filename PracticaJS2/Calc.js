6260186230c1716870717f78c1bccbd145d6e4cf
JCQuiroga <josecquiroga@gmail.com>
2015-09-29 13:11:05 +0200

    Terminado

---
+++ PracticaJS2/Calc.js
@@ -0,0 +1,95 @@
+﻿var res;
+
+function suma() {
+    var op1 = document.getElementById("operando1");
+    var op2 = document.getElementById("operando2");
+    res = parseInt(op1.value) + parseInt(op2.value);
+
+    var resultado = document.getElementById("resultado");
+    if (isNaN(op1.value) || isNaN(op2.value) || isNaN(res)) {
+        resultado.innerHTML = "Error. Introduce valores adecuados";
+    } else {
+        resultado.innerHTML = res;
+    }
+
+    var igual = document.getElementById("igual");
+    igual.innerHTML = "=";
+
+    var operador = document.getElementById("operador");
+    operador.innerHTML = "+";
+}
+
+function resta() {
+    var op1 = document.getElementById("operando1");
+    var op2 = document.getElementById("operando2");
+    res = parseInt(op1.value) - parseInt(op2.value);
+
+    var resultado = document.getElementById("resultado");
+    if (isNaN(op1.value) || isNaN(op2.value) || isNaN(res)) {
+        resultado.innerHTML = "Error. Introduce valores adecuados";
+    } else {
+        resultado.innerHTML = res;
+    }
+
+    var igual = document.getElementById("igual");
+    igual.innerHTML = "=";
+
+    var operador = document.getElementById("operador");
+    operador.innerHTML = "-";
+}
+
+function mult() {
+    var op1 = document.getElementById("operando1");
+    var op2 = document.getElementById("operando2");
+    res = parseInt(op1.value) * parseInt(op2.value);
+
+    var resultado = document.getElementById("resultado");
+    if (isNaN(op1.value) || isNaN(op2.value) || isNaN(res)) {
+        resultado.innerHTML = "Error. Introduce valores adecuados";
+    } else {
+        resultado.innerHTML = res;
+    }
+
+    var igual = document.getElementById("igual");
+    igual.innerHTML = "=";
+
+    var operador = document.getElementById("operador");
+    operador.innerHTML = "*";
+}
+
+function div() {
+    var op1 = document.getElementById("operando1");
+    var op2 = document.getElementById("operando2");
+    res = parseInt(op1.value) / parseInt(op2.value);
+
+    var resultado = document.getElementById("resultado");
+    if (isNaN(op1.value) || isNaN(op2.value) || isNaN(res)) {
+        resultado.innerHTML = "Error. Introduce valores adecuados";
+    } else {
+        resultado.innerHTML = res;
+    }
+
+    var igual = document.getElementById("igual");
+    igual.innerHTML = "=";
+
+    var operador = document.getElementById("operador");
+    operador.innerHTML = "/";
+}
+
+
+function borrar() {
+    var op1 = document.getElementById("operando1");
+    op1.value = "";
+
+    var op2 = document.getElementById("operando2");
+    op2.value = "";
+
+    var resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+
+    var igual = document.getElementById("igual");
+    igual.innerHTML = "";
+
+    var operador = document.getElementById("operador");
+    operador.innerHTML = "<br />";
+}
---
+++ PracticaJS2/CalcCorreccion.js
@@ -0,0 +1,59 @@
+﻿/*
+//Empleando una función para cada botón. Es algo redundante, pero más sencillo
+var suma = function () {
+    var n1 = parseFloat(document.getElementById("txtN1").value);
+    var n2 = parseFloat(document.getElementById("txtN2").value);
+    var res = n1 + n2;
+
+    document.getElementById("lbOpe").innerHTML = "+";
+    document.getElementById("lbTot").innerHTML = res;
+}
+
+var resta = function () {
+    var n1 = parseFloat(document.getElementById("txtN1").value);
+    var n2 = parseFloat(document.getElementById("txtN2").value);
+    var res = n1 - n2;
+
+    document.getElementById("lbOpe").innerHTML = "-";
+    document.getElementById("lbTot").innerHTML = res;
+}
+
+var multiplicar = function () {
+    var n1 = parseFloat(document.getElementById("txtN1").value);
+    var n2 = parseFloat(document.getElementById("txtN2").value);
+    var res = n1 * n2;
+
+    document.getElementById("lbOpe").innerHTML = "*";
+    document.getElementById("lbTot").innerHTML = res;
+}
+
+var dividir = function () {
+    var n1 = parseFloat(document.getElementById("txtN1").value);
+    var n2 = parseFloat(document.getElementById("txtN2").value);
+    var res = n1 / n2;
+
+    document.getElementById("lbOpe").innerHTML = "/";
+    document.getElementById("lbTot").innerHTML = res.toFixed(2); //Limita a 2 decimales
+}
+
+    document.getElementById("btnMas").onclick = suma;
+    document.getElementById("btnMenos").onclick = resta;
+    document.getElementById("btnPor").onclick = multiplicar;
+    document.getElementById("btnDividir").onclick = dividir;
+*/
+
+//Esta funcion reduce todas las funciones anteriores a una. Con eval() analiza la expresión y la evalua
+
+    var operar = function (e) { 
+        var n1 = parseFloat(document.getElementById("txtN1").value);
+        var n2 = parseFloat(document.getElementById("txtN2").value);
+        var operador = e.target.outerText; //Con este método tomamos lo que hay escrito en el boton, que en este caso está acoplado. Si no lo estuviese, habría que hacer un switch o algo asi.
+        var res = eval(n1 + operador + n2);
+        document.getElementById("lbOpe").innerHTML = operador;
+        document.getElementById("lbTot").innerHTML = res.toFixed(2); 
+    }
+
+    document.getElementById("btnMas").onclick = operar;
+    document.getElementById("btnMenos").onclick = operar;
+    document.getElementById("btnPor").onclick = operar;
+    document.getElementById("btnDividir").onclick = operar;
